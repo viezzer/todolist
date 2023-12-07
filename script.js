aaec8e0cafe79ac9994db6b16bbf0e765d2ff2ee
eduviezzer <33733608+viezzer@users.noreply.github.com>
2023-12-07 15:02:32 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById("input-box")
+const listContainer = document.getElementById("list-container")
+
+function addTask(){
+    if (inputBox.value.trim() === '') {
+        alert("Você deve escrever alguma coisa!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span")
+        span.innerHTML="\u00d7";
+        li.appendChild(span)
+        saveData()
+    }
+    inputBox.value="";
+}
+
+listContainer.addEventListener("click", (e) => {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked")
+        saveData()
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove()
+        saveData()
+    }
+})
+
+function saveData() {
+    localStorage.setItem("tasks", listContainer.innerHTML)
+}
+
+function getTasks() {
+    listContainer.innerHTML = localStorage.getItem("tasks")
+}
+
+getTasks()
